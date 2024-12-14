377ad628d82362234ef650d32a37f32e5be8af56
sharifabee <sharifabee.alur@gmail.com>
2024-12-14 12:07:12 +0530

    remove elements

--- RemoveDuplicateEle.java
+++ RemoveDuplicateEle.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
-public class Excercise4 {
+public class RemoveDuplicateEle {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Remove duplicate elements in a given String
+		
+// welcome to full stack development
+		// welcom t fu stak 
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter data");
 		String data = sc.nextLine();
-		//String data = "welcome to full stack development";
 		data = data.toUpperCase();
 		
 		int[] count = {
@@ -15,18 +19,20 @@ public class Excercise4 {
 				};
 		char ch = ' ';
 		int index;
+		String result = "";
+		
 		for(int i=0;i<data.length();i++) {
 			ch = data.charAt(i);
 			if(ch >='A' && ch <='Z') {
 				index = (int)(ch -65);
 				count[index]++;// 'A'  65 % 65
-			}
-			
-		}
-		for(int i=0;i<26;i++) {
-			if(count[i] > 0)
-			System.out.println("["+(char)(i+65)+"]--->"+count[i]);
-		}
+				if(count[index] == 1) {
+					result = result + ch;	
+				}// if
+			}// if
+		}// for
+		
+	System.out.println("result------>"+result);
 	}
 
 }
