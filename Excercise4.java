ea4b101e6c4c4dfb2338475964dfceb7efc4af96
sharifabee <sharifabee.alur@gmail.com>
2024-12-14 11:28:28 +0530

    no of elements

--- Excercise4.java
+++ Excercise4.java
@@ -8,26 +8,20 @@ public class Excercise4 {
 		data = data.toUpperCase();
 		
 		int[] count = {
-				0,0,0,0,0,
-				0,0,0,0,0,
-				0,0,0,0,0,
-				0,0,0,0,0,
-				0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 				};
-	
-		
 		char ch = ' ';
 		int index;
 		for(int i=0;i<data.length();i++) {
 			ch = data.charAt(i);
 			if(ch >='A' && ch <='Z') {
 				index = (int)(ch -65);
-				System.out.print(index+" ");
 				count[index]++;// 'A'  65 % 65
 			}
 		}
 		for(int i=0;i<26;i++) {
-			System.out.println((char)(i+65)+" no of elements--->"+count[i]);
+			if(count[i] > 0)
+			System.out.println("["+(char)(i+65)+"]--->"+count[i]);
 		}
 		
 	
